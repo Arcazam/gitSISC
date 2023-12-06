aea41afcce50b9818fbd7910cd0b27c6ee63c557
Arcazam <qwer0469@naver.com>
2023-12-06 09:41:16 +0900

    commit Sisc/Day2/231206

--- SISC/src/main/java/board/model/BoardDao.java
+++ SISC/src/main/java/board/model/BoardDao.java
@@ -1,5 +1,7 @@
 package board.model;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,5 +13,53 @@ public class BoardDao {
 	SqlSessionTemplate sqlSessionTemplate;
 	
 	private String namespace="board.";
-	
+
+	// (메인화면)자유게시판
+	public List<BoardBean> getAllFreeBoard() {
+		List<BoardBean> free_board_list = sqlSessionTemplate.selectList(namespace+"getAllFreeBoard");
+		return free_board_list;
+	}
+
+	// (메인화면)qna게시판
+	public List<BoardBean> getAllKnowBoard() {
+		List<BoardBean> know_board_list = sqlSessionTemplate.selectList(namespace+"getAllKnowBoard");
+		return know_board_list;
+	}
+
+	// (메인화면)qna게시판
+	public List<BoardBean> getAllQnABoard() {
+		List<BoardBean> qna_board_list = sqlSessionTemplate.selectList(namespace+"getAllQnABoard");
+		return qna_board_list;
+	}
+
+	// (메인화면)수료생게시판
+	public List<BoardBean> getAllGradBoard() {
+		List<BoardBean> grad_board_list = sqlSessionTemplate.selectList(namespace+"getAllGradBoard");
+		return grad_board_list;
+	}
+
+	// 자유게시판 레코드 숫자
+	public int getCountFree() {
+		int freeBoardCount = sqlSessionTemplate.selectOne(namespace+"getCountFree");
+		return freeBoardCount;
+	}
+
+	// 지식게시판 레코드 숫자
+	public int getCountKnow() {
+		int knowBoardCount = sqlSessionTemplate.selectOne(namespace+"getCountKnow");
+		return knowBoardCount;
+	}
+
+	// QnA게시판 레코드 숫자
+	public int getCountQnA() {
+		int qnaBoardCount = sqlSessionTemplate.selectOne(namespace+"getCountQnA");
+		return qnaBoardCount;
+	}
+
+	// (메인화면)수료생게시판
+	public int getCountGrad() {
+		int gradBoardCount = sqlSessionTemplate.selectOne(namespace+"getCountGrad");
+		return gradBoardCount;
+	}
+
 }
--- SISC/src/main/java/book/model/BookDao.java
+++ SISC/src/main/java/book/model/BookDao.java
@@ -1,5 +1,7 @@
 package book.model;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,5 +13,17 @@ public class BookDao {
 	SqlSessionTemplate sqlSessionTemplate;
 	
 	private String namespace="book.";
+
+	// 책 레코드 5개출력
+	public List<BookBean> selectAllToMainBook() {
+		List<BookBean> book_list = sqlSessionTemplate.selectList(namespace+"selectAllToMainBook");
+		return book_list;
+	}
+
+	// 책 레코드 숫자출력
+	public int getCountBook() {
+		int bookCount = sqlSessionTemplate.selectOne(namespace+"getCountBook");
+		return bookCount;
+	}
 	
 }
