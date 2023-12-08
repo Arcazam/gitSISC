f62668fc26c191b66182a38d17f0cc2daca9fdc8
nam <rudgus4655@naver.com>
2023-12-08 10:40:35 +0900

    3개병합

---
+++ SISC/src/main/java/view/controller/GoMainController.java
@@ -0,0 +1,16 @@
+package view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoMainController {
+	private final String command = "SiscMain.view";
+	private final String gotoPage = "Home";
+	
+	@RequestMapping(command)
+	public String goMain() {
+		
+		return gotoPage;
+	}
+}
